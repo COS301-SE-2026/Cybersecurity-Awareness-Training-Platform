34b38e438cbbd7b0079bc2b26c8d2ef85e49eeeb
u24569608 <u24569608@tuks.co.za>
2026-05-19 17:04:10 +0200

    feat: update auth-context for real login

--- apps/frontend/src/context/auth-context.ts
+++ apps/frontend/src/context/auth-context.ts
@@ -1,9 +1,16 @@
 import { createContext } from 'react';
 
+export type AuthUser = {
+  firstName: string;
+  lastName: string;
+  email: string;
+};
+
 export type AuthContextType = {
   isAuthenticated: boolean;
   token: string | null;
-  login: (token: string) => void;
+  user: AuthUser | null;
+  login: (token: string, user: AuthUser) => void;
   logout: () => void;
 };
 
