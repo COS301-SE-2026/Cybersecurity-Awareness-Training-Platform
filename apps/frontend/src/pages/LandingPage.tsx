9d449ea4d46f3644301520346adc9777f681e509
u24569608 <u24569608@tuks.co.za>
2026-06-22 17:39:55 +0200

    feat: hero section

--- apps/frontend/src/pages/LandingPage.tsx
+++ apps/frontend/src/pages/LandingPage.tsx
@@ -1,10 +1,15 @@
 import LandingNavbar from '../components/layout/LandingNavbar';
+import HeroSection from '../components/landing-page/HeroSection';
 
 function LandingPage() {
   return (
     <main>
       {/* NAVBAR */}
       <LandingNavbar />
+      <div className="pt-32">
+        {/* HERO SECTION */}
+        <HeroSection />
+      </div>
     </main>
   );
 }
