c98f9c62d0ede9437e844aca240d7c84c50ff64e
u24569608 <u24569608@tuks.co.za>
2026-06-25 15:35:47 +0200

    feat: forgot password page skeleton

--- apps/frontend/src/pages/ForgotPasswordPage.tsx
+++ apps/frontend/src/pages/ForgotPasswordPage.tsx
@@ -1,5 +1,63 @@
 function ForgotPasswordPage() {
-  return <main></main>;
+  return (
+    <section className="bg-light-purple dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        {/* LOGO  */}
+        <div className="mb-4 flex items-center space-x-3 rtl:space-x-reverse">
+          <img src="/Phish Logo Light.png" className="h-14" alt="Insightful Phish Logo" />
+          <span className="flex items-center gap-2 mt-2">
+            <span className="font-overpass self-center text-[1.94rem] text-pink text-heading font-medium whitespace-nowrap tracking-wide">
+              Insightful
+            </span>
+            <span className="font-overpass self-center text-[1.94rem] text-pink text-heading font-black whitespace-nowrap tracking-wide">
+              Phish.
+            </span>
+          </span>
+        </div>
+
+        <div className="w-full p-6 bg-white-purple shadow dark:border md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700 sm:p-8">
+          {/* HEADING */}
+          <h3 className="font-jost text-3xl text-purple tracking-wider font-medium text-heading">
+            Forgot your Password?
+          </h3>
+
+          {/* SUB-HEADING */}
+          <p className="font-regular tracking-wide text-[1.1rem] font-justify font-jost mt-1 text-dark-pink">
+            Enter the email address associated with your account and we'll send you a link to reset
+            your password.
+          </p>
+
+          <form className="mt-4 space-y-4 lg:mt-5 md:space-y-5" action="#">
+            {/* EMAIL INPUT */}
+            <div>
+              <label
+                htmlFor="email"
+                className=" block mb-2 font-jost tracking-wide text-xl font-medium text-gray-900 text-dark-pink"
+              >
+                Email Address
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="font-overpass text-[1.2rem] bg-gray-50 border border-gray-300 text-deep-purple focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Enter your Email Address"
+                required
+              />
+            </div>
+
+            {/* RESET PASSWORD BUTTON */}
+            <button
+              type="submit"
+              className="cursor-pointer w-full inline-flex items-center justify-center text-white font-jost text-[1.2rem] font-regular tracking-wider bg-main-purple hover:bg-hover-purple box-border border border-transparent focus:ring-4 focus:ring-brand-medium shadow-xs leading-5 text-sm px-4 py-2.5 focus:outline-none"
+            >
+              <span> Send Password Reset Link </span>
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default ForgotPasswordPage;
