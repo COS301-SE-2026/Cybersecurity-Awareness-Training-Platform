42558ad5316cfe1a1781b2fa2686178f2072490d
u24569608 <u24569608@tuks.co.za>
2026-06-29 14:50:08 +0200

    chore: forgot password page unit testing (TEST 2 and TEST 3)
    Test 2: Missing Email Address (No Email Address)
    Test 3: Invalid Email Address

--- apps/frontend/src/pages/testing/ForgotPasswordPage.test.tsx
+++ apps/frontend/src/pages/testing/ForgotPasswordPage.test.tsx
@@ -15,11 +15,41 @@ describe('ForgotPasswordPage', () => {
     );
 
     expect(screen.getByRole('heading', { name: /Forgot your Password?/i })).toBeInTheDocument();
-
     expect(screen.getByPlaceholderText(/Enter your Email Address/i)).toBeInTheDocument();
-
     expect(screen.getByRole('button', { name: /Send Password Reset Link/i })).toBeInTheDocument();
-
     expect(screen.getByRole('link', { name: /Back to Login/i })).toBeInTheDocument();
+  });
+
+  // Test 2: Empty Email Validation
+  it('shows a validation message when no email is entered', async () => {
+    render(
+      <MemoryRouter>
+        <ForgotPasswordPage />
+      </MemoryRouter>,
+    );
+
+    await userEvent.click(screen.getByRole('button', { name: /Send Password Reset Link/i }));
+
+    expect(screen.getByText(/Please Enter An Email Address/i)).toBeInTheDocument();
+  });
+
+  // Test 3: Invalid Email Validation
+  it('shows a validation message for an invalid email address', async () => {
+    render(
+      <MemoryRouter>
+        <ForgotPasswordPage />
+      </MemoryRouter>,
+    );
+
+    // Type/Enter Invalid Email
+    await userEvent.type(
+      screen.getByPlaceholderText(/Enter your Email Address/i),
+      'adrianorobertodacostajorge',
+    );
+
+    // Click Send Password Reset Link Button
+    await userEvent.click(screen.getByRole('button', { name: /Send Password Reset Link/i }));
+
+    expect(screen.getByText(/Please Enter A Valid Email Address/i)).toBeInTheDocument();
   });
 }); // describe
