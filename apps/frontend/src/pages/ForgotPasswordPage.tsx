2b8e17ab6f7e071052ab28b4308d4c2f7ed3cb83
u24569608 <u24569608@tuks.co.za>
2026-06-29 13:52:55 +0200

    feat: forgot password success and loading UI

--- apps/frontend/src/pages/ForgotPasswordPage.tsx
+++ apps/frontend/src/pages/ForgotPasswordPage.tsx
@@ -6,6 +6,9 @@ import { useState } from 'react';
 function ForgotPasswordPage() {
   const [email, setEmail] = useState('');
   const [alertMessage, setAlertMessage] = useState('');
+  const [showSuccess, setShowSuccess] = useState(false);
+  const [successMessage, setSuccessMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -23,13 +26,23 @@ function ForgotPasswordPage() {
       return;
     }
 
-    console.log('Validation Passed');
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+      setShowSuccess(true);
+      setSuccessMessage(
+        'If An Account Exists For This Email Address, Password Instructions Have Been Sent',
+      );
+    }, 2000);
   }
   return (
     <section className="bg-light-purple dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         {/* LOGO  */}
         {alertMessage && <BasicAlert variant="danger">{alertMessage}</BasicAlert>}
+        {successMessage && <BasicAlert variant="success">{successMessage}</BasicAlert>}
+
         <div className="mb-4 flex items-center space-x-3 rtl:space-x-reverse">
           <img src="/Phish Logo Light.png" className="h-14" alt="Insightful Phish Logo" />
           <span className="flex items-center gap-2 mt-2">
@@ -77,9 +90,29 @@ function ForgotPasswordPage() {
             {/* RESET PASSWORD BUTTON */}
             <button
               type="submit"
+              disabled={isLoading}
               className="cursor-pointer w-full inline-flex items-center justify-center text-white font-jost text-[1.2rem] font-regular tracking-wider bg-main-purple hover:bg-hover-purple box-border border border-transparent focus:ring-4 focus:ring-brand-medium shadow-xs leading-5 text-sm px-4 py-2.5 focus:outline-none"
             >
-              <span> Send Password Reset Link </span>
+              {isLoading && (
+                <svg
+                  aria-hidden="true"
+                  className="mr-3 h-5 w-5 animate-spin fill-white text-white/30"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                >
+                  <path
+                    d="M100 50.6C100 78.2 77.6 100.6 50 100.6C22.4 100.6 0 78.2 0 50.6C0 23 22.4 0.6 50 0.6C77.6 0.6 100 23 100 50.6Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.97 39.04C96.39 38.4 97.86 35.91 97.01 33.55C95.29 28.82 92.87 24.37 89.82 20.35C85.84 15.12 80.88 10.72 75.21 7.41C69.54 4.1 63.27 1.94 56.77 1.05C51.77 0.37 46.7 0.45 41.73 1.28C39.26 1.69 37.81 4.2 38.45 6.62C39.08 9.04 41.57 10.47 44.05 10.11C47.85 9.56 51.72 9.53 55.54 10.23C60.86 11 65.99 12.78 70.63 15.47C75.27 18.16 79.33 21.7 82.58 25.84C84.91 28.81 86.8 32.13 88.18 35.68C89.08 38.01 91.54 39.68 93.97 39.04Z"
+                    fill="currentFill"
+                  />
+                </svg>
+              )}
+              <span>
+                {isLoading ? 'Sending Password Reset Link...' : 'Send Password Reset Link'}
+              </span>
             </button>
 
             <Link
