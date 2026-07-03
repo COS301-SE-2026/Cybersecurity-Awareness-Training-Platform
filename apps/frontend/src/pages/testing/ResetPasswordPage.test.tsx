1cae7ee3caf4cc342b84282e3e63a061877d0efc
u24569608 <u24569608@tuks.co.za>
2026-07-03 15:00:18 +0200

    chore: password reset page testing (TEST 4 and TEST 5)
    Test 4: Password Mismatch Validation (Must Show Validation Message)
    Test 5: Show Loading State (Successful Password Reset)

--- apps/frontend/src/pages/testing/ResetPasswordPage.test.tsx
+++ apps/frontend/src/pages/testing/ResetPasswordPage.test.tsx
@@ -54,4 +54,50 @@ describe('ResetPasswordPage', () => {
 
     expect(screen.getByText(/Password Must Be At Least 12 Characters Long/i)).toBeInTheDocument();
   });
+
+  // Test 4: Password Mismatch Validation
+  it('shows a validation message when the passwords do not match', async () => {
+    render(
+      <MemoryRouter>
+        <ResetPasswordPage />
+      </MemoryRouter>,
+    );
+
+    // Enter a  Password
+    await userEvent.type(screen.getByPlaceholderText(/Enter a New Password/i), 'Password-1-Test@');
+
+    // Confirm Password
+    await userEvent.type(screen.getByPlaceholderText(/Re-Enter New Password/i), 'Password-2-Test#');
+
+    // "Press" Reset Password Button
+    await userEvent.click(screen.getByRole('button', { name: /Reset Password/i }));
+
+    expect(screen.getByText(/Password Confirmation Must Match Password/i)).toBeInTheDocument();
+  });
+
+  // Test 5: Loading State
+  it('shows the loading state after submitting a valid password', async () => {
+    render(
+      <MemoryRouter>
+        <ResetPasswordPage />
+      </MemoryRouter>,
+    );
+
+    // Enter a  Password
+    await userEvent.type(
+      screen.getByPlaceholderText(/Enter a New Password/i),
+      'Password-1-Test@!Connor-Was-Here!',
+    );
+
+    // Confirm Password
+    await userEvent.type(
+      screen.getByPlaceholderText(/Re-Enter New Password/i),
+      'Password-1-Test@!Connor-Was-Here!',
+    );
+
+    // "Press" Reset Password Button
+    await userEvent.click(screen.getByRole('button', { name: /Reset Password/i }));
+
+    expect(screen.getByRole('button', { name: /Resetting Password.../i })).toBeDisabled();
+  });
 });
