ade4f60fb3bc403fc1b6522d2afd50e31c20a8d6
u24569608 <u24569608@tuks.co.za>
2026-07-07 13:38:49 +0200

    feat: account management page (PART 1)

---
+++ apps/frontend/src/components/account-management/AccountSettingsPage.tsx
@@ -0,0 +1,108 @@
+function AccountSettingsPage() {
+  return (
+    <div className="-mt-2 -ml-2">
+      {/* HEADING */}
+      <h3 className="font-jost text-2xl text-dark-pink tracking-wider font-medium">
+        Account Settings
+      </h3>
+
+      {/* SUB-HEADING */}
+      <p className="font-regular tracking-wider text-[1.1rem] font-justify font-jost mt-1 text-gray-500">
+        Manage the settings associated with your account.
+      </p>
+      <p className="font-regular tracking-wider text-[1.1rem] font-justify font-jost -mt-1 text-gray-500 mb-6">
+        Update your email address, password, or delete your account.
+      </p>
+
+      <form className="mt-4 grid grid-cols-2 gap-6" noValidate>
+        {/* Email Address */}
+        <div>
+          <label
+            htmlFor="email-address"
+            className=" block mb-2 font-jost tracking-wide text-xl font-medium text-pink"
+          >
+            Email Address
+            {/* <span className="font-light text-red-500">(Required)</span> */}
+          </label>
+          <div className="flex items-end gap-2">
+            <input
+              required
+              type="email"
+              name="email-address"
+              disabled
+              value="email@example.com"
+              id="email-address"
+              className="disabled:opacity-50 font-overpass text-[1.2rem] bg-gray-50 border border-gray-300 text-deep-purple focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Enter your Email Address"
+            />
+
+            <button
+              type="button"
+              className="w-150 cursor-pointer inline-flex items-center justify-center text-white font-jost text-[1.2rem] font-regular tracking-wider bg-main-purple hover:bg-red-700 box-border border border-transparent focus:ring-4 focus:ring-brand-medium shadow-xs leading-5 text-sm py-3 focus:outline-none disabled:opacity-60 disabled:cursor-not-allowed"
+            >
+              <span className="material-symbols-sharp mr-4 ">edit</span>
+              <span> Change Email </span>
+            </button>
+          </div>
+        </div>
+
+        {/* Password*/}
+        <div>
+          <label
+            htmlFor="password"
+            className=" block mb-2 font-jost tracking-wide text-xl font-medium text-pink"
+          >
+            Password
+            {/* <span className="font-light text-red-500">(Required)</span> */}
+          </label>
+          <div className="flex items-end gap-2">
+            <input
+              required
+              type="password"
+              name="password"
+              disabled
+              value="1234567891234"
+              id="password"
+              className="disabled:opacity-50 font-overpass text-[1.2rem] bg-gray-50 border border-gray-300 text-deep-purple focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Enter your Password"
+            />
+
+            <button
+              type="button"
+              className="w-150 cursor-pointer inline-flex items-center justify-center text-white font-jost text-[1.2rem] font-regular tracking-wider bg-main-purple hover:bg-main-purple box-border border border-transparent focus:ring-4 focus:ring-brand-medium shadow-xs leading-5 text-sm px-4 py-3 focus:outline-none disabled:opacity-60 disabled:cursor-not-allowed"
+            >
+              <span className="material-symbols-sharp mr-4">edit</span>
+              <span> Change Password </span>
+            </button>
+          </div>
+        </div>
+      </form>
+
+      {/* HEADING */}
+      <h3 className="font-jost text-[1.3rem] text-red-600 tracking-wider font-medium mt-10">
+        Danger Zone
+      </h3>
+
+      {/* SUB-HEADING */}
+      <p className="font-regular tracking-wider text-[1.1rem] font-justify font-jost mt-1 text-red-500">
+        Permanently delete your <em>Insightful Phish</em> account and all associated data.
+      </p>
+      <p className="font-regular tracking-wider text-[1.1rem] font-justify font-jost -mt-1 mb-4 text-red-500">
+        Once your account is deleted, it cannot be recovered.
+      </p>
+
+      <div className="mt-2 flex items-center justify-between">
+        {/* Next Button */}
+        <button
+          type="button"
+          className="cursor-pointer px-6 inline-flex gap-2 items-center justify-center text-white font-jost text-[1.2rem] font-regular tracking-wider bg-red-600 hover:bg-red-700 box-border border border-transparent focus:ring-4 focus:ring-brand-medium shadow-xs leading-5 text-sm px-4 py-2.5 focus:outline-none disabled:opacity-60 disabled:cursor-not-allowed"
+        >
+          <span className="material-symbols-sharp">delete</span>
+          <span> Delete Account </span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default AccountSettingsPage;
---
+++ apps/frontend/src/pages/AccountManagementPage.tsx
@@ -0,0 +1,67 @@
+import AppLayout from '../components/layout/AppLayout';
+import PersonalSettingsPage from '../components/account-management/PersonalSettingsPage';
+import AccountSettingsPage from '../components/account-management/AccountSettingsPage';
+
+function AccountManagementPage() {
+  return (
+    <AppLayout
+      contentStyle={{
+        backgroundColor: '#F3F4F6',
+      }}
+    >
+      {/* HEADING */}
+      <div
+        style={{
+          padding: '1.4rem',
+          paddingBottom: '0.8rem',
+          boxSizing: 'border-box',
+          flexShrink: 0,
+        }}
+      >
+        <h1
+          style={{
+            margin: 0,
+            marginBottom: '1.6rem',
+            fontSize: '3.8rem',
+            fontWeight: 500,
+            lineHeight: 1,
+            // color: 'white',
+            color: 'rgb(132, 25, 255)',
+            fontFamily: 'Jost',
+          }}
+        >
+          Account Management
+        </h1>
+      </div>
+
+      <div className="flex flex-col flex-1 p-5 -mt-5 w-full">
+        {/* TAB BUTTONS */}
+        <ul className="hidden text-sm font-medium text-center text-body sm:flex -space-x-px">
+          <li className="w-full focus-within:z-10">
+            <button className="font-jost inline-block w-full bg-neutral-primary-soft border border-default hover:bg-neutral-secondary-medium hover:text-[var(--ip-purple)] focus:ring-4 focus:ring-neutral-secondary-strong font-light text-[1.2rem] tracking-wide leading-5 px-5 py-3 focus:outline-none">
+              Personal Information
+            </button>
+          </li>
+          <li className="w-full focus-within:z-10">
+            <button className="font-jost inline-block w-full bg-neutral-primary-soft border border-default hover:bg-neutral-secondary-medium hover:text-[var(--ip-purple)] focus:ring-4 focus:ring-neutral-secondary-strong font-light text-[1.2rem] tracking-wide leading-5 px-5 py-3 focus:outline-none">
+              Account
+            </button>
+          </li>
+          <li className="w-full focus-within:z-10">
+            <button className="font-jost inline-block w-full bg-neutral-primary-soft border border-default hover:bg-neutral-secondary-medium hover:text-[var(--ip-purple)] focus:ring-4 focus:ring-neutral-secondary-strong font-light text-[1.2rem] tracking-wide leading-5 px-5 py-3 focus:outline-none">
+              Sessions
+            </button>
+          </li>
+        </ul>
+
+        {/* CONTENT BOX */}
+        <div className="w-full p-6 bg-white md:mt-0 bg-neutral-primary-soft border-default border-x border-b">
+          {/* <PersonalSettingsPage /> */}
+          <AccountSettingsPage />
+        </div>
+      </div>
+    </AppLayout>
+  );
+}
+
+export default AccountManagementPage;
