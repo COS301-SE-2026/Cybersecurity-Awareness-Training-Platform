02a74b5196d00bb8f8de27f35bdf5e374ca49709
u24569608 <u24569608@tuks.co.za>
2026-07-14 18:13:46 +0200

    fix: wording

--- apps/frontend/src/components/organisation-information/OrganisationTimelinePage.tsx
+++ apps/frontend/src/components/organisation-information/OrganisationTimelinePage.tsx
@@ -3,13 +3,13 @@ function OrganisationTimelinePage() {
     <div className="-mt-2 -ml-2">
       {/* HEADING */}
       <h3 className="font-jost text-2xl text-dark-pink tracking-wider font-medium">
-        Organisation Timeline
+        Organisation Event Timeline
       </h3>
 
       {/* SUB-HEADING */}
       <p className="font-regular tracking-wider text-[1.1rem] font-justify font-jost text-gray-500 mb-6">
         View the chronological history of organisation registration, onboarding, and platform
-        actions.
+        events.
       </p>
 
       {/* <div className="flex flex-col flex-1 w-full grid grid-cols-3 gap-6">
