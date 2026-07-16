94efc375f78f2e1d844b253b464d08fc3af2dcd5
u24569608 <u24569608@tuks.co.za>
2026-07-16 14:17:32 +0200

    fix: sonarcube duplicate code
    I made 5 of the same hardcoded timeline events, for the sake of design. SonarCube was NOT impressed!

--- apps/frontend/src/components/organisation-information/OrganisationTimelinePage.tsx
+++ apps/frontend/src/components/organisation-information/OrganisationTimelinePage.tsx
@@ -58,98 +58,18 @@ function OrganisationTimelinePage() {
               <div className="mt-3 sm:pe-8">
                 {/* Date and Time */}
                 <time className="bg-neutral-secondary-medium border border-default-medium text-[0.9rem] font-regular text-gray-600 font-overpass px-1.5 pt-1 py-0.5">
-                  9 January 2025, 06:00 PM
+                  10 January 2025, 07:00 PM
                 </time>
 
                 {/* Event Title */}
                 <h3 className="text-lg max-w-3xs font-medium text-pink my-2 tracking-wider font-jost">
-                  Organisation Request Submitted
+                  Organisation Request Declined
                 </h3>
 
                 {/* Event Description */}
                 <p className="text-body max-w-3xs mb-4 tracking text-dark-pink font-overpass text-[0.95rem]">
-                  The organisation registration request was submitted by the representative.
-                </p>
-              </div>
-            </li>
-
-            {/* TIMELINE ITEM/EVENT 3 */}
-            <li className="relative mb-6 sm:mb-0">
-              <div className="flex items-center">
-                {/* Dot */}
-                <div className="z-10 flex items-center justify-center w-3 h-3 bg-main-purple rounded-full ring-0 ring-buffer sm:ring-8 shrink-0"></div>
-                {/* Line */}
-                <div className="hidden sm:flex w-full bg-gray-300 h-px"></div>
-              </div>
-
-              <div className="mt-3 sm:pe-8">
-                {/* Date and Time */}
-                <time className="bg-neutral-secondary-medium border border-default-medium text-[0.9rem] font-regular text-gray-600 font-overpass px-1.5 pt-1 py-0.5">
-                  9 January 2025, 06:00 PM
-                </time>
-
-                {/* Event Title */}
-                <h3 className="text-lg max-w-3xs font-medium text-pink my-2 tracking-wider font-jost">
-                  Organisation Request Submitted
-                </h3>
-
-                {/* Event Description */}
-                <p className="text-body max-w-3xs mb-4 tracking text-dark-pink font-overpass text-[0.95rem]">
-                  The organisation registration request was submitted by the representative.
-                </p>
-              </div>
-            </li>
-
-            {/* TIMELINE ITEM/EVENT 4 */}
-            <li className="relative mb-6 sm:mb-0">
-              <div className="flex items-center">
-                {/* Dot */}
-                <div className="z-10 flex items-center justify-center w-3 h-3 bg-main-purple rounded-full ring-0 ring-buffer sm:ring-8 shrink-0"></div>
-                {/* Line */}
-                <div className="hidden sm:flex w-full bg-gray-300 h-px"></div>
-              </div>
-
-              <div className="mt-3 sm:pe-8">
-                {/* Date and Time */}
-                <time className="bg-neutral-secondary-medium border border-default-medium text-[0.9rem] font-regular text-gray-600 font-overpass px-1.5 pt-1 py-0.5">
-                  9 January 2025, 06:00 PM
-                </time>
-
-                {/* Event Title */}
-                <h3 className="text-lg max-w-3xs font-medium text-pink my-2 tracking-wider font-jost">
-                  Organisation Request Submitted
-                </h3>
-
-                {/* Event Description */}
-                <p className="text-body max-w-3xs mb-4 tracking text-dark-pink font-overpass text-[0.95rem]">
-                  The organisation registration request was submitted by the representative.
-                </p>
-              </div>
-            </li>
-
-            {/* TIMELINE ITEM/EVENT 5 */}
-            <li className="relative mb-6 sm:mb-0">
-              <div className="flex items-center">
-                {/* Dot */}
-                <div className="z-10 flex items-center justify-center w-3 h-3 bg-main-purple rounded-full ring-0 ring-buffer sm:ring-8 shrink-0"></div>
-                {/* Line */}
-                <div className="hidden sm:flex w-full bg-gray-300 h-px"></div>
-              </div>
-
-              <div className="mt-3 sm:pe-8">
-                {/* Date and Time */}
-                <time className="bg-neutral-secondary-medium border border-default-medium text-[0.9rem] font-regular text-gray-600 font-overpass px-1.5 pt-1 py-0.5">
-                  9 January 2025, 06:00 PM
-                </time>
-
-                {/* Event Title */}
-                <h3 className="text-lg max-w-3xs font-medium text-pink my-2 tracking-wider font-jost">
-                  Organisation Request Submitted
-                </h3>
-
-                {/* Event Description */}
-                <p className="text-body max-w-3xs mb-4 tracking text-dark-pink font-overpass text-[0.95rem]">
-                  The organisation registration request was submitted by the representative.
+                  The organisation registration request was declined by the{' '}
+                  <em>Insightful Phish</em> platform administrator.
                 </p>
               </div>
             </li>
