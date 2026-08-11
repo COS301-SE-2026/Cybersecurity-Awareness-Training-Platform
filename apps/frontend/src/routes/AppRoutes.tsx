ddce1b499de5d20f15cf69f45d433e1a0a785698
u24569608 <u24569608@tuks.co.za>
2026-08-11 13:33:22 +0200

    fix: routes

--- apps/frontend/src/routes/AppRoutes.tsx
+++ apps/frontend/src/routes/AppRoutes.tsx
@@ -136,7 +136,7 @@ function AppRoutes() {
       <Route path="*" element={<Navigate to="/" replace />} />
       <Route path="/brand" element={<BrandPage />} />
 
-      <Route path="/campaign-assignment" element={<CampaignAssignmentPage />} />
+      {/* <Route path="/campaign-assignment" element={<CampaignAssignmentPage />} /> */}
       <Route
         element={
           <ProtectedRoute
@@ -147,7 +147,7 @@ function AppRoutes() {
         }
       >
         <Route
-          path="/organisations/:organisationId/campaign-assignment/new"
+          path="/organisations/:organisationId/campaign-assignments/new"
           element={<CampaignAssignmentPage />}
         />
       </Route>
