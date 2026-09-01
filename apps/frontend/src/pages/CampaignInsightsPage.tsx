e5fc32758cf3362c1aad5d50651a5f2814369972
u24569608 <u24569608@tuks.co.za>
2026-09-01 10:29:24 +0200

    fix: duplicate code

--- apps/frontend/src/pages/CampaignInsightsPage.tsx
+++ apps/frontend/src/pages/CampaignInsightsPage.tsx
@@ -1,40 +1,6 @@
 import AppLayout from '../components/layout/AppLayout';
 import { useState } from 'react';
-
-type DisplayStatus =
-  | 'Active'
-  | 'Disabled'
-  | 'Invited'
-  | 'Failed to Send'
-  | 'Accepted'
-  | 'Completed'
-  | 'Expired'
-  | 'Revoked'
-  | 'Rejected'
-  | 'Unknown';
-
-function getStatusBadge(status: DisplayStatus) {
-  const variants: Record<DisplayStatus, string> = {
-    Active: 'ring-success-subtle text-fg-success-strong bg-success-soft',
-    Disabled: 'ring-default-medium text-heading bg-neutral-secondary-medium',
-    Invited: 'ring-brand-subtle text-fg-brand-strong bg-brand-softer',
-    'Failed to Send': 'ring-danger-subtle text-fg-danger-strong bg-danger-soft',
-    Accepted: 'ring-success-subtle text-fg-success-strong bg-success-soft',
-    Completed: 'ring-success-subtle text-fg-success-strong bg-success-soft',
-    Expired: 'ring-default-medium text-heading bg-neutral-secondary-medium',
-    Revoked: 'ring-danger-subtle text-fg-danger-strong bg-danger-soft',
-    Rejected: 'ring-warning-subtle text-fg-warning bg-warning-soft',
-    Unknown: 'ring-default-medium text-fg-heading bg-neutral-secondary-medium',
-  };
-
-  return (
-    <span
-      className={`items-flex justify-center items-center w-32 px-4 py-1 pt-[0.4rem] ring-2 ring-inset text-sm font-medium ${variants[status]}`}
-    >
-      {status}
-    </span>
-  );
-}
+import StatusBadge from '../components/ui/StatusBadge';
 
 function CampaignInsightsPage() {
   const [isLoading] = useState(false);
@@ -83,7 +49,9 @@ function CampaignInsightsPage() {
               <p className="font-regular tracking-wider text-[1.1rem] font-justify font-jost font-medium text-dark-pink mt-4 mb-1">
                 Status
               </p>
-              <div className="font-overpass tracking-wider">{getStatusBadge('Active')}</div>
+              <div className="font-overpass tracking-wider">
+                <StatusBadge status="Active" />
+              </div>
             </div>
 
             <div>
@@ -330,7 +298,9 @@ function CampaignInsightsPage() {
                     </td>
 
                     {/* Status Badge */}
-                    <td className="px-6 py-2">{getStatusBadge('Active')}</td>
+                    <td className="px-6 py-2">
+                      <StatusBadge status="Active" />
+                    </td>
 
                     {/* Actions Dropdown */}
                     <td className="px-6 py-2">
